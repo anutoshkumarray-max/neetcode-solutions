17508fb87ac058bae9a45fe432b1672931fb0d8a
Anutosh Kumar Ray <anutoshkumarray@gmail.com>
2026-09-19 01:22:09 +0530

    Add: continuous-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int prefixsum = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for(int i=0;i<n;i++){
+            prefixsum += nums[i];
+            int rem = prefixsum % k;
+            if(map.containsKey(rem)){
+                if(i-map.get(rem)>=2){
+                    return true;
+                }
+            }else{
+                map.put(rem,i);
+            }
+        }
+        return false;
+    }
+}
